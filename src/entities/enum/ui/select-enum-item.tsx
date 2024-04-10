0ffc5e011dc2fceb4ea24c1cd815ab130b3940e1
Bogdan Safronov <bogdasafronov@gmail.com>
2024-04-10 14:43:28 +0300

    feat(enum): add forwarding ref

--- src/entities/enum/ui/select-enum-item.tsx
+++ src/entities/enum/ui/select-enum-item.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { forwardRef } from "react";
+import { type ForwardedRef, forwardRef } from "react";
 import { type ControllerRenderProps } from "react-hook-form";
 import { api } from "~/shared/lib/trpc/react";
 import { MySelect } from "~/shared/ui-my/my-select";
@@ -9,9 +9,20 @@ type Props = ControllerRenderProps & {
   enumId: ID;
 };
 
-export const SelectEnumItem = forwardRef(({ enumId, ...props }: Props) => {
-  const { data } = api.enumItem.findMany.useQuery({ enumId });
-  return <MySelect options={data} by="id" {...props} />;
-});
+export const SelectEnumItem = forwardRef(
+  ({ enumId, ...props }: Props, _ref: ForwardedRef<HTMLButtonElement>) => {
+    const { data } = api.enumItem.findMany.useQuery({ enumId });
+    return <MySelect options={data} by="id" {...props} />;
+  },
+);
 
 SelectEnumItem.displayName = "SelectEnumItem";
+
+export const SelectEnumItemByName = forwardRef(
+  ({ enumId, ...props }: Props, _ref: ForwardedRef<HTMLButtonElement>) => {
+    const { data } = api.enumItem.findMany.useQuery({ enumId });
+    return <MySelect options={data} by="name" {...props} />;
+  },
+);
+
+SelectEnumItemByName.displayName = "SelectEnumItemByName";
